53cb565c7887fb1a17d99de26b19bc49471ea226
boyuanx <boyuanxu@usc.edu>
2022-04-12 01:41:34 -0700

    Optimized mapping getter

--- contracts/EthSignPublicEncryptionKeyRegistry.sol
+++ contracts/EthSignPublicEncryptionKeyRegistry.sol
@@ -4,18 +4,14 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 contract EthSignPublicEncryptionKeyRegistry is ERC2771Context {
-    mapping(address => string) internal _registry;
+    mapping(address => string) public registry;
 
     event Registered(address entity, string publicEncryptionKey);
 
     constructor(address forwarder) ERC2771Context(forwarder) {}
 
     function register(string calldata publicEncryptionKey) external {
-        _registry[_msgSender()] = publicEncryptionKey;
+        registry[_msgSender()] = publicEncryptionKey;
         emit Registered(_msgSender(), publicEncryptionKey);
-    }
-
-    function getKey(address entity) external view returns (string memory) {
-        return _registry[entity];
     }
 }
